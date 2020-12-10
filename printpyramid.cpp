3fb840f5cf94872b53b0cc9fdb0adb79d1bdbc5d
Mirza-Ahmad <70958597+Mirza-Ahmad@users.noreply.github.com>
2020-12-10 18:32:11 +0500

    Added new project

---
+++ printpyramid.cpp
@@ -0,0 +1,133 @@
+#include<iostream>
+using namespace std;
+int main(void)
+{
+for(int i=1; i<=15; i++)
+{
+for(int j=1; j<=15; j++)
+{
+if(i==1 || i==15 || j==1 || j==15)
+{
+cout<<"*";	
+}
+else if(i==2 && j==14)
+{
+cout<<"*";
+}
+else if(i==3 && j==13)
+{
+cout<<"*";
+}
+else if(i==4 && j==12)
+{
+cout<<"*";
+}
+else if(i==5 && j==11)
+{
+cout<<"*";
+}
+else if(i==6 && j==10)
+{
+cout<<"*";
+}
+else if(i==7 && j==9)
+{
+cout<<"*";
+}
+else if(i==8 && j==8)
+{
+cout<<"*";
+}
+else if(i==9 && j==7)
+{
+cout<<"*";
+}
+else if(i==10 && j==6)
+{
+cout<<"*";
+}
+else if(i==11 && j==5)
+{
+cout<<"*";
+}
+else if(i==12 && j==4)
+{
+cout<<"*";
+}
+else if(i==13 && j==3)
+{
+cout<<"*";
+}
+else if(i==14 && j==2)
+{
+cout<<"*";
+}
+else if(i==2 && j==2)
+{
+cout<<"*";
+}
+else if(i==3 && j==3)
+{
+cout<<"*";
+}
+else if(i==4 && j==4)
+{
+cout<<"*";
+}
+else if(i==5 && j==5)
+{
+cout<<"*";
+}
+else if(i==6 && j==6)
+{
+cout<<"*";
+}
+else if(i==7 && j==7)
+{
+cout<<"*";
+}
+else if(i==8 && j==8)
+{
+cout<<"*";
+}
+else if(i==8 && j==8)
+{
+cout<<"*";
+}
+else if(i==9 && j==9)
+{
+cout<<"*";
+}
+else if(i==10 && j==10)
+{
+cout<<"*";
+}
+else if(i==11 && j==11)
+{
+cout<<"*";
+}
+else if(i==12 && j==12)
+{
+cout<<"*";
+}
+else if(i==13 && j==13)
+{
+cout<<"*";
+}
+else if(i==14 && j==14)
+{
+cout<<"*";
+}
+else if(i==8 || j==8)
+{
+cout<<"*";
+}
+else
+{
+cout<<" ";
+}
+}
+cout<<endl;	
+}
+return 0;
+}
